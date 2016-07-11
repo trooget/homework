7cd6f1bad4f13fa69d46a01ad94ae8448216b028
--replace-all <zaichenko_oleg.overlord@mail.ru>
2016-07-11 18:01:33 +0300

    My menu hook begin

---
+++ first_menu.api.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * hook first_menu menu
+ * @param $route
+ * @param $item
+ */
+function hook_first_menu_menu($route, $item) {
+
+}
+
+function hook_first_menu_menu_alter(&$subitems, $route, $item){
+
+}
